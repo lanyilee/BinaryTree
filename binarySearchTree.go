1a5e68d0b0c2948be8bbd173cd2e2204f946e6aa
lanyilee <2269146219@qq.com>
2018-12-02 22:25:04 +0800

    delete1

--- binarySearchTree.go
+++ binarySearchTree.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 )
 
 type tree struct {
@@ -48,6 +49,48 @@ func (t *tree) searchNode(num int) {
 		}
 	}
 	fmt.Println("can not find it")
+}
+
+func (t *tree) deleteNode(num int) {
+	var fatherTree *tree
+	for {
+		if t == nil {
+			break
+		}
+		if num == t.data {
+			fmt.Println("father node :" + strconv.Itoa(fatherTree.data))
+			// find the data which should be deleted
+			if t.leftTree == nil && t.rightTree == nil {
+				if fatherTree.leftTree == t {
+					fatherTree.leftTree = nil //delete
+				} else if fatherTree.rightTree == t {
+					fatherTree.rightTree = nil //delete
+				}
+			} else if t.leftTree != nil && t.rightTree == nil {
+				if fatherTree.leftTree == t {
+					fatherTree.leftTree = t.leftTree // the deleted data contains only the left node
+				} else if fatherTree.rightTree == t {
+					fatherTree.rightTree = t.leftTree
+				}
+			} else if t.leftTree == nil && t.rightTree != nil {
+				if fatherTree.leftTree == t {
+					fatherTree.leftTree = t.rightTree //the deleted data contains only the right node
+				} else if fatherTree.rightTree == t {
+					fatherTree.rightTree = t.rightTree
+				}
+			} else if t.leftTree != nil && t.rightTree != nil {
+				// find the smallest data in the deleted rightTree
+
+			}
+
+		} else if num < t.data {
+			fatherTree = t
+			t = t.leftTree
+		} else if num > t.data {
+			fatherTree = t
+			t = t.rightTree
+		}
+	}
 }
 
 func (t *tree) infixOrder() {
